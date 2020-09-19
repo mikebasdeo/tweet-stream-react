4307ea0fc9f8dfc3943efef7ec35529debd0c468
Mike Basdeo <mikebasdeo@gmail.com>
2020-09-19 15:39:32 -0400

    tweets showing

--- src/Tweets.js
+++ src/Tweets.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
 
-import Tweet from './Tweet';
+import Tweet from './Tweet'
 
 const Tweets = ({ tweets = [] }) => {
+  useEffect(() => {
+    console.log('hello sir')
+  }, [])
   return (
     <section className="Tweets">
       {tweets.map((tweet) => (
         <Tweet key={tweet.id} tweet={tweet} />
       ))}
     </section>
-  );
-};
+  )
+}
 
-export default Tweets;
+const mapStateToProps = ({ tweets }) => ({ tweets })
+export default connect(mapStateToProps)(Tweets)
--- src/actions.js
+++ src/actions.js
@@ -1,11 +1,17 @@
+export const ADD_TWEETS = 'ADD_TWEETS'
+
+// Thunk'd action creator -> calls regular redux action when promise resolves
 export const fetchTweets = () => {
   return (dispatch) => {
     // 💰 ways to hit an API endpoint - (fetch api, axios, jquery.ajax)
     fetch('http://tweet-stream.glitch.me/api/tweets')
       .then((response) => response.json())
       .then((response) => {
-        console.log('made it here')
-        console.log(response, dispatch)
+        // console.log('made it here')
+        // console.log(response, dispatch)
+        dispatch(addTweets(response.tweets))
       })
   }
 }
+
+export const addTweets = (tweets) => ({ type: ADD_TWEETS, payload: { tweets } })
